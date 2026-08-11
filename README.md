182fbe0d90fde2ae5c640983c3f694bae174a1c7
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-11 11:05:38 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# UEFI snake game
+This is a simple snake game that runs in the UEFI. Test in QEMU by running run.sh. Make bootable usb by copying esp/ directory onto root of a FAT formatted partition.
--- src/constants.rs
+++ src/constants.rs
@@ -12,16 +12,12 @@ pub const TEXT_COL: BltPixel = BltPixel::new(255, 255, 255);
 pub const BACKGROUND_COL: BltPixel = BltPixel::new(0, 0, 0);
 
 // Difficulty levels
-pub const DIFFICULTIES: [Difficulty; 9] = [
+pub const DIFFICULTIES: [Difficulty; 5] = [
     Difficulty { name: "Snail", tick_rate: 6 },
     Difficulty { name: "Easy", tick_rate: 10 },
     Difficulty { name: "'mid'", tick_rate: 15 },
     Difficulty { name: "Hard", tick_rate: 20 },
     Difficulty { name: "Even Harder", tick_rate: 40 },
-    Difficulty { name: "No.", tick_rate: 80 },
-    Difficulty { name: "Stop.", tick_rate: 120 },
-    Difficulty { name: "Don't You Dare", tick_rate: 200 },
-    Difficulty { name: "Why?", tick_rate: 500 },
 ];
 
 pub const RENDER_FPS: usize = 120;
